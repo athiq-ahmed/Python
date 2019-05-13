4b2489b3483cd929608012bb46d9094dac593d20
Athiq <42245686+athiqahmed@users.noreply.github.com>
2019-05-13 17:08:29 +0530

    Add files via upload

---
+++ Basic operators.py
@@ -0,0 +1,49 @@
+# =============================================================================
+# https://www.tutorialspoint.com/python/python_basic_operators.htm
+# =============================================================================
+
+#Types of Operator
+#Python language supports the following types of operators.
+#    Arithmetic Operators
+#    Comparison (Relational) Operators
+#    Assignment Operators
+#    Logical Operators
+#    Bitwise Operators
+#    Membership Operators
+#    Identity Operators
+
+#Python Arithmetic Operators
+a = 10
+b = 20
+
+a + b
+a - b
+a * b
+a / b
+a % b  # % Modulus - Divides left hand operand by right hand operand and returns remainder
+a ** b # ** Exponent - Performs exponential (power) calculation on operators
+a // b # Floor Division - The division of operands where the result is the quotient in which the digits after the decimal point are removed. But if one of the operands is negative, the result is floored, i.e., rounded away from zero (towards negative infinity) −
+
+
+#Python Comparison Operators
+a = 10
+b = 20
+
+a == b  # equal to
+a != b  # not equal to
+a > b
+a < b
+a >= b
+a <= b
+
+#Python assignment operators
+a = 10
+b = 20
+c = 30
+
+c = a + b
+c += a
+c -= a
+c *= a 
+c /= a
+
